6cb95d02c734425ecc8545f9e8d73cb9d3a7df62
Daniel Lewan <vision360.daniel@gmail.com>
2016-05-03 01:36:17 +0200

    override basic methods in terminal script - draw and minimum size This allow to custom control drawing
    Docs about custom drawing http://docs.godotengine.org/en/latest/tutorials/2d/custom_drawing_in_2d.html

--- addons/terminal/terminal.gd
+++ addons/terminal/terminal.gd
@@ -1,7 +1,15 @@
-tool
+
 extends Control
 
 func _ready():
-	pass
+	update()
 
+func _draw():
+	draw_rect(get_rect(), Color(1,1,1,1))
 
+func get_minimum_size(): # override
+	# TODO
+	return Vector2(100,100)
+	
+func _on_resize(): # signal
+	prints("Size ",get_size())
