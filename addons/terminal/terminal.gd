48bfe5107b8fd07ebe26d313f4333a8343f30384
Daniel Lewan <vision360.daniel@gmail.com>
2016-05-03 14:55:15 +0200

    create export variables and some private variables

--- addons/terminal/terminal.gd
+++ addons/terminal/terminal.gd
@@ -7,8 +7,25 @@ export(int) var min_rows = 1
 
 export (DynamicFont) var dynamicFont
 
+# offset of characters in cells
+export(int) var font_x_offset = 0
+export(int) var font_y_offset = 0
+
+# change the size of cells
+# The size of the cells is calculated based on size of "W" character
+# These properties allow you to change the margin of characters
+export(int) var resize_cell_x = 0
+export(int) var resize_cell_y = 0
+
+
+export(Color, RGB) var foregound_default  # default text color
+export(Color, RGB) var background_default # default background color
+
 # private variables
 var font
+
+var grid = Vector2() # rows and collumns
+var cell = Vector2() # cell size in pixels
 
 func _ready():
 	font = dynamicFont
