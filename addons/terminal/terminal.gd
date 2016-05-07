b3da74d17515f5ff58bd327cff30e7b50652307c
Daniel Lewan <vision360.daniel@gmail.com>
2016-05-08 01:47:15 +0200

    Add dynamic size calculation (in rows and collumns)

--- addons/terminal/terminal.gd
+++ addons/terminal/terminal.gd
@@ -27,17 +27,38 @@ var font
 var grid = Vector2() # rows and collumns
 var cell = Vector2() # cell size in pixels
 
+var buffer
+
 func _ready():
 	font = dynamicFont
+	assert(font != null)
+	var Buffer = preload("res://addons/terminal/buffer.gd")
+#	buffer = Buffer.new(grid,foregound_default,background_default," ")
+	calculate_size()
 	update()
 
 func _draw():
 	draw_rect(get_rect(), background_default)
 
+# Calculate the grid size. Final result depens of font size
+func calculate_size():
+	if font == null: # this was called by _on_resize signal before font was loaded
+		return
+	var width = get_size().width
+	var height = get_size().height
+	
+	cell.width = int(font.get_string_size("W").width) + resize_cell_x
+	cell.height = int(font.get_height()) + resize_cell_y
+	
+	grid.width = ( width - (int(width) % int(cell.width)) ) / cell.width
+	grid.height = ( height - (int(height) % int(cell.height)) ) / cell.height
+	prints(grid)
+
 func get_minimum_size(): # override
 	# TODO
 	return Vector2(100,100)
-	
+
+# Call manually when changed font size
 func _on_resize(): # signal
 	prints("Size ",get_size())
-
+	calculate_size()
