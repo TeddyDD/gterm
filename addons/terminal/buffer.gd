b9be1058a38b49a4b9a59533c5a2008828e05e85
Daniel Lewan <vision360.daniel@gmail.com>
2016-05-08 01:25:14 +0200

    add base buffer class

---
+++ addons/terminal/buffer.gd
@@ -0,0 +1,39 @@
+extends Reference
+
+var size # size of buffer: Vector2
+
+var chars    # array of chars
+var fgcolors # foreground (text) colors
+var bgcolors # background colors
+
+# Create buffer of given size (Vector2D) and fill with default values
+# empty_char by default is " " (space)
+func _init(size, fg, bg, empty_char):
+	if not empty_char:
+		empty_char == " "
+	size = size
+		# initialize arrays
+	chars = []
+	fgcolors = []
+	bgcolors = []
+	set_default(" ",fg,bg)
+
+# returns index for given column and row
+func index(point):
+	var column = point.x
+	var row = point.y
+	return ((row-1) * size.width) + (column - 1)
+	
+func set_default(c, fg, bg):
+	# resize buffers
+	var b = size.width * size.height
+	
+	chars.resize(b)
+	fgcolors.resize(b)
+	bgcolors.resize(b)
+	
+	# set default variables
+	for item in range( b ):
+		chars[item] = c
+		fgcolors[item] = fg
+		bgcolors[item] = bg
